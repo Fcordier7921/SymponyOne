3b521242c1edeb77e545e98b08f2578f1a1dfb88
Fcordier7921 <f.cordier@codeur.online>
2021-01-19 16:19:14 +0100

    started

---
+++ src/Controller/PortefolioController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PortefolioController extends AbstractController
+{
+    /**
+     * @Route("/portefolio", name="portefolio")
+     */
+    public function index(): Response
+    {
+        return $this->render('portefolio/index.html.twig', [
+            'controller_name' => 'PortefolioController',
+        ]);
+    }
+    
+    /**
+     * @Route("/", name= "home")
+     */
+
+    public function home(){
+        return $this->render('portefolio/home.html.twig');
+    }
+}
